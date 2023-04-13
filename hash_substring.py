bcbd86b23415974699cb9e80c20d2ca734004fed
Roberts Kristofers Strautiņš <124698215+RobyTheHardGamer@users.noreply.github.com>
2023-04-13 15:36:07 +0000

    eee

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,46 @@
-# python3
+import sys
 
-def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+print("Enter 'I' to input from keyboard, or 'F' to input from file")
+choice = input().strip()
 
-def print_occurrences(output):
-    # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output)))
+input1 = ""
+input2 = ""
+if choice == "I":
+    print("Enter pattern:")
+    input1 = input().strip()
 
-def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    print("Enter text:")
+    input2 = input().strip()
 
-    # and return an iterable variable
-    return [0]
+elif choice == "F":
+    print("Enter file name: ")
+    try:
+        fileName = input().strip()
+        with open(fileName, 'r') as f:
+            input1 = f.readline().strip()
+            input2 = f.readline().strip()
+            print("Firstline is : " + input1)
+            print("Secondline is : " + input2)
+    except:
+        print("error")
+        sys.exit(0)
+else:
+    print("wrong input")
 
+len1 = len(input1)
+len2 = len(input2)
 
-# this part launches the functions
-if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
-
+i = 0
+x = len1
+y = 0
+str = input2
+while i < len2:
+    try:
+        str2 = str[y:x]
+        x += 1
+        y += 1
+        if str2 == input1:
+            print(i, end=" ")
+        i += 1
+    except:
+        break
